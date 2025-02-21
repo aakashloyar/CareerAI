be7469999c43c07a8c5163c113d986ed7b80d0b7
aakashloyar <aakashloyar@gmail.com>
2025-02-21 16:33:15 +0530

    added providers and middleware

--- components/Appbar.tsx
+++ components/Appbar.tsx
@@ -1,15 +1,33 @@
+"use client"
+import {signIn,signOut} from 'next-auth/react'
+import Button from '@/components/button'
+import Link from "next/link";
+
 export default function AppBar(){
     return (
       <div className='bg-black w-full h-14 text-white flex flex-col justify-center border-b-1 border-slate-100 fixed'>
         <div className='grid grid-cols-12'>
-          <div className='col-span-2 text-2xl font-bold flex justify-center'>
-            CarrerAI
+          <div className='col-span-2 flex items-center justify-center'>
+            <Link href="/">
+            <button className="">
+              <div className='text-2xl font-bold '>
+                CarrerAI
+              </div>
+            </button>
+            </Link>
           </div>
+        
+          
           <div className='col-span-6'>
   
           </div>
           <div className='col-span-4 flex justify-center'>
-            Signin
+          <div>
+            <Button handleClick={() => signIn()} label={'Sign In'}/>
+            </div>
+            <div>
+            <Button handleClick={() => signOut()} label={'SignOut'}/>  
+            </div>
           </div>
         </div>
       </div>
