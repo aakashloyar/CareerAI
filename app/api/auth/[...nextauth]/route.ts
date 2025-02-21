2c68f43c08ff36786a1cbb6746e6e53dab589886
aakashloyar <aakashloyar@gmail.com>
2025-02-21 22:35:55 +0530

    zod init

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,7 @@ import GoogleProvider from "next-auth/providers/google";
 import NextAuth from 'next-auth'
 import CredentialsProvider from "next-auth/providers/credentials";
 import GithubProvider from "next-auth/providers/github"
+import { userSchema } from "@/lib/validation";
 
 const handler=NextAuth({
     providers: [
@@ -33,7 +34,25 @@ const handler=NextAuth({
                 }
               }
             })
-    ]
+    ],
+    callbacks: {
+      async jwt({ token, user }:{token:JWT,user:any}) {
+        if (user) {
+          token.id = user.id;
+          token.firstName = user.firstName;
+          token.lastName = user.lastName;
+        }
+        return token;
+      },
+      async session({ session, token }:{session:Session,token:JWT}) {
+        if (session.user) {
+          session.user.id = token.id;
+          session.user.firstName = token.firstName;
+          session.user.lastName = token.lastName;
+        }
+        return session;
+      },
+    }
     ,
     pages:{
       signIn:'/auth/signin'
@@ -43,3 +62,20 @@ const handler=NextAuth({
 })
 
 export {handler as GET, handler as POST}
+
+
+
+interface Session {
+  user: {
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+  };
+}
+
+interface JWT {
+  id: string;
+  firstName: string;
+  lastName: string;
+}
---
+++ lib/validation.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const userSchema = z.object({
+  firstname: z.string().min(2, "First name must be at least 2 characters"),
+  lastname: z.string().min(2, "Last name must be at least 2 characters"),
+  email: z.string().email("Invalid email format"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+});
+
+export type UserSchema = z.infer<typeof userSchema>;
